dd9c48a3beafbd4d7f53998e0febf04c769c6e46
Kevin Gege <kcampodonico@unprg.edu.pe>
2025-07-08 18:47:43 -0500

    feat: Add media resources, styles, and AppDoubleText widget
    - Created AppMedia class to manage image assets.
    - Introduced AppStyles class for consistent styling across the app, including primary color, background color, text colors, and various text styles.
    - Implemented AppDoubleText widget for displaying two text elements with customizable styles and tap functionality.

---
+++ lib/base/res/media.dart
@@ -0,0 +1,5 @@
+class AppMedia{
+  static const _baseImage = "assets/images/";
+
+  static const logo = '$_baseImage/logo_ticket_app.png';
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import "package:fluentui_icons/fluentui_icons.dart";
 import "package:flutter/material.dart";
+import "package:ticket_app/base/res/media.dart";
+import "package:ticket_app/base/res/styles/app_styles.dart";
+import "package:ticket_app/base/widgets/app_double_text.dart";
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +10,63 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: AppStyles.bgColor,
+      body: ListView(
+        children: [
+          SizedBox(height: 40),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good morning", style: AppStyles.headLineStyle3),
+                        SizedBox(height: 6),
+                        Text("Book Tickets", style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(AppMedia.logo),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFF4F6FD),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(FluentSystemIcons.ic_fluent_search_regular),
+                      Text("Search"),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40),
+                AppDoubleText(
+                  bigText: 'Upcoming Flights',
+                  smallText: 'View All',
+                ),
+              ],
+            ),
+          ),
+          /* SizedBox(height: 40,),
+          AppDoubleText(bigText: 'Upcoming Flights', smallText: 'View All',), */
+        ],
+      ),
     );
   }
 }
